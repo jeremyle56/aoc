e4d3413f12d87b98117161dde9ac744b098685fa
Jeremy Le <jeremyle420@gmail.com>
2023-12-02 15:58:24 +1100

    edited template and add python

---
+++ 2023/template.py
@@ -0,0 +1,26 @@
+import collections, math, sys
+
+###############################################################################
+
+
+def solve_p1():
+    res = 0
+    print(f"Part 1: {res}")
+
+
+###############################################################################
+
+
+def solve_p2():
+    res = 0
+    print(f"Part 2: {res}")
+
+
+###############################################################################
+
+
+lines = open("input.txt").read().splitlines()
+# with open("input.txt") as file:
+#    lines = [line.rstrip() for line in f]
+solve_p1()
+solve_p2()
