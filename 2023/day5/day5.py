9cf09a5f90fe10abeecf7fac1f0510a563bab41c
Jeremy Le <jeremyle420@gmail.com>
2023-12-05 23:16:06 +1100

    day 5 attempt part 2

--- 2023/day5/day5.py
+++ 2023/day5/day5.py
@@ -29,28 +29,38 @@ def solve_p1():
 def solve_p2():
     seedsRange = [seeds[i : i + 2] for i in range(0, len(seeds), 2)]
 
-    seedsMap = {}
+    seedsMap = []
     for [start, length] in seedsRange:
         for i in range(start, start + length):
-            seedsMap[i] = i
+            seedsMap.append(i)
 
-    moved = dict((i, False) for i in seedsMap)
+    reverse = lines
+    reverse.reverse()
 
-    for line in lines:
-        if re.match(r"[^0-9 ]", line):
-            moved = dict.fromkeys(moved, False)
-            print(seedsMap)
-            continue
+    loc = 10000000
+    moved = False
+    currPos = 0
 
-        line = [int(i) for i in line.split()]
-        [dest, src, length] = line
+    while True:
+        for line in reverse:
+            if re.match(r"[^0-9 ]", line):
+                moved = False
+                continue
 
-        for key, value in seedsMap.items():
-            if value >= src and value < src + length and not moved[value]:
-                seedsMap[key] = dest + (value - src)
-                moved[dest + (value - src)] = True
+            line = [int(i) for i in line.split()]
+            [dest, src, length] = line
+            if currPos >= dest and currPos < dest + length and not moved:
+                currPos = src + (currPos - dest)
+                moved = True
 
-    print(f"Answer for Part 2: {min(seedsMap.values())}")
+        if currPos in seedsMap or loc == 88151870:
+            break
+        else:
+            loc += 1
+            moved = False
+            currPos = loc
+
+    print(f"Answer for Part 2: {loc}")
 
 
 ###############################################################################
