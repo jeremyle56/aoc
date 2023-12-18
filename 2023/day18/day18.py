6fade51526cbdb641634ec086f81d08e1e6dd525
Jeremy Le <jeremyle420@gmail.com>
2023-12-18 18:36:25 +1100

    Refactored Day 18 2023 Solution

--- 2023/day18/day18.py
+++ 2023/day18/day18.py
@@ -1,97 +1,40 @@
 import sys
-import numpy as np
 
-
-def get_max_size(s):
-    return sum(int(i[1]) for i in plan if i[0] == s)
-
-
-def mark_grid(grid, amount, x, y, dx, dy):
-    for _ in range(amount):
-        grid[x + dx][y + dy] = "#"
-        x += dx
-        y += dy
-    return (x, y)
-
-
-# https://stackoverflow.com/questions/41077185/fastest-way-to-shoelace-formula
-def shoelace(x_y):
-    x_y = np.array(x_y)
-    x_y = x_y.reshape(-1, 2)
-
-    x = x_y[:, 0]
-    y = x_y[:, 1]
-
-    s1 = np.sum(x * np.roll(y, -1))
-    s2 = np.sum(y * np.roll(x, -1))
-
-    return 0.5 * np.abs(s1 - s2)
-
+dirs = {"U": [-1, 0], "D": [1, 0], "L": [0, -1], "R": [0, 1]}
+directions = ["R", "D", "L", "U"]
 
 ###############################################################################
 
 
-def solve_p1():
-    grid = [["." for _ in range(max)] for _ in range(max)]
-    grid[0][0] = "#"
+def solve(part2):
     cur = (0, 0)
-    pos = []
-    pos.append(cur)
+    points = [cur]
+    perim = 0
 
-    for i in plan:
-        match i[0]:
-            case "U":
-                cur = mark_grid(grid, i[1], cur[0], cur[1], -1, 0)
-            case "D":
-                cur = mark_grid(grid, i[1], cur[0], cur[1], 1, 0)
-            case "L":
-                cur = mark_grid(grid, i[1], cur[0], cur[1], 0, -1)
-            case "R":
-                cur = mark_grid(grid, i[1], cur[0], cur[1], 0, 1)
-        pos.append(cur)
+    for i in hex if part2 else plan:
+        diff_x, diff_y = dirs[i[0]]
+        perim += i[1]
+        cur = (cur[0] + diff_x * i[1], cur[1] + diff_y * i[1])
+        points.append(cur)
 
-    # for i in grid:
-    #     for j in i:
-    #         print(j, end="")
-    #     print()
+    area = 0
+    for i in range(len(points) - 1):
+        area += (points[i][0] * points[i + 1][1]) - (points[i + 1][0] * points[i][1])
 
-    return shoelace(pos[:-1]) + sum(i[1] for i in plan) // 2 + 1
-
-
-###############################################################################
-
-
-def solve_p2():
-    hex = [i[2].replace("#", "") for i in plan]
-    hex = [[int(i[-1]), i[:5]] for i in hex]
-    direction = ["R", "D", "L", "U"]
-    p2 = [[direction[i[0]], int(i[1], 16)] for i in hex]
-    cur = (0, 0)
-    pos = [cur]
-
-    for i in p2:
-        match i[0]:
-            case "U":
-                cur = (cur[0] - i[1], cur[1])
-            case "D":
-                cur = (cur[0] + i[1], cur[1])
-            case "L":
-                cur = (cur[0], cur[1] - i[1])
-            case "R":
-                cur = (cur[0], cur[1] + i[1])
-        pos.append(cur)
-
-    return shoelace(pos[:-1]) + sum(i[1] for i in p2) // 2 + 1
+    return perim // 2 + abs(area) // 2 + 1
 
 
 ###############################################################################
 
 input_file = "test.txt" if "test" in sys.argv else "input.txt"
 lines = open(input_file).read().splitlines()
-temp = [i.replace("(", "").replace(")", "").split() for i in lines]
-plan = [[x, int(y), z] for x, y, z in temp]
+temp = [i.split() for i in lines]
+plan = [[x, int(y)] for x, y, _ in temp]
 
-max = max(get_max_size("U"), get_max_size("D"), get_max_size("L"), get_max_size("R"))
+temp1 = [i[2][2:-1] for i in temp]
+print(temp1)
+hex = [[directions[int(i[-1])], int(i[:5], 16)] for i in temp1]
 
-print(f"Answer for Part 1: {solve_p1()}")
-print(f"Answer for Part 2: {solve_p2()}")
+
+print(f"Answer for Part 1: {solve(False)}")
+print(f"Answer for Part 2: {solve(True)}")
