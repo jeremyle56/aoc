aaa39e09342008bd3d2587c662b3c455c94d6c32
Jeremy Le <jeremyle420@gmail.com>
2024-12-15 21:59:16 +1100

    feat(04-24): learned pygyat

--- 2024/newday.sh
+++ 2024/newday.sh
@@ -2,16 +2,23 @@
 
 ## Creates a folder for a problem, using $1
 # "template" directory must be in the same directory
-# example: ./newday.sh 01
+# Default language is Rust
+# example: ./newday.sh 01 pygyat
 
-if [ $# != 1 ]; then
-    echo "Usage: $0 <day number>"
+if [ $# -le 1 ]; then
+    echo "Usage: $0 <day number> <language>"
     exit 1
 fi;
 
 # Create the folder from template
-cargo new "day${1}"
-cp template.rs "./day${1}/src/main.rs"
+if [ "$2" = "pygyat" ]; then
+    mkdir "day${1}" || exit
+    cp template.gyat "day$1/day$1.gyat"
+else
+    cargo new "day${1}"
+    cp template.rs "./day${1}/src/main.rs"
+fi
 
-# Create empty input file
+# Create empty input and test file
 touch "./day${1}/in.txt"
+touch "./day${1}/test.txt"
